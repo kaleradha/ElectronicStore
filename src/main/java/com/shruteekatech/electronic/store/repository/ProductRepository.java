292c630d573d3ec3ae873e5926f4204c105a2a10
radha <kaleradha>
2023-04-04 22:57:44 +0530

    Done product custome Methods!

---
+++ src/main/java/com/shruteekatech/electronic/store/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.shruteekatech.electronic.store.repository;
+
+import com.shruteekatech.electronic.store.entity.Product;
+import com.shruteekatech.electronic.store.helper.PageableResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+  Page<Product> findByTitleContaining(String subTitle, Pageable pageable);
+
+    Page<Product>findByliveTrue(Pageable pageable);
+}
---
+++ src/main/java/com/shruteekatech/electronic/store/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.shruteekatech.electronic.store.service;
+
+import com.shruteekatech.electronic.store.dto.ProductDto;
+import com.shruteekatech.electronic.store.helper.PageableResponse;
+
+public interface ProductService {
+
+    ProductDto createProduct(ProductDto productDto);
+
+    ProductDto updateProduct(ProductDto productDto ,long productId);
+
+    ProductDto getProduct(long productId);
+
+    void deleteProduct(long productId);
+    PageableResponse<ProductDto>getAll(int pageSize, int pageNumber, String sortBy, String sortDir);
+
+    PageableResponse<ProductDto>getAllLive(int pageSize, int pageNumber, String sortBy, String sortDir);
+
+    PageableResponse<ProductDto>searchProductByTitle(String subTitle,int pageSize, int pageNumber, String sortBy, String sortDir);
+
+
+}
